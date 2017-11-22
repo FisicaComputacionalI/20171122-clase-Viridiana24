b205574af98020a3543374e078a7e32fc1649996
Viridiana <31377504+Viridiana24@users.noreply.github.com>
2017-11-22 13:26:49 -0600

    Add files via upload

---
+++ clase.cc
@@ -0,0 +1,21 @@
+//Programa 1
+//Barranco Diaz Carmen Viridiana
+//22 noviembre 2017
+//Iniciamos el programa
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+   //Declaramos nuestras variables
+   float x;
+   float y;
+//Como queremos calcular el valor de la funcion cos(x) 
+	cout<<"Dame el valor de x "<<endl;
+cin>>x;
+
+y=cos(x);
+
+	cout<<"EL VALOR ES "<<y;
+return 0;
+}
